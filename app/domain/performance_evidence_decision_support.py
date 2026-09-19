bf1e0b0c4624c7cdbca03e76a56d904d98c08bad
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-19 03:23:43 +0300

    Repair evidence posture enum syntax

--- app/domain/performance_evidence_decision_support.py
+++ app/domain/performance_evidence_decision_support.py
@@ -22,13 +22,13 @@ class InferentialStatus(str, Enum):
 
 
 class CombinedEvidencePosture(str, Enum):
-    DESCRIPTIVE_CHANGE_WITH_STATISTICAL_DETECTION = (
-        "descriptive_change_with_statistical_detection"
-    )
-    DESCRIPTIVE_CHANGE_WITHOUT_STATISTICAL_DETECTION = (
+    DESCRIPTIVE_CHANGE_WITH_STATISTICAL_DETECTION = "descriptive_change_with_statistical_detection"
+    DESCRIPTIVE_CHANGE_WITHOUT_STATISTICAL_DETECTION = "descriptive_change_without_statistical_detection"
     DESCRIPTIVE_AND_STATISTICAL_DISAGREEMENT = "descriptive_and_statistical_disagreement"
-        "descriptive_change_without_statistical_detection"
-    )
+    NO_DESCRIPTIVE_CHANGE = "no_descriptive_change"
+    INFERENTIAL_EVIDENCE_UNAVAILABLE = "inferential_evidence_unavailable"
+    CONTEXT_INVALID = "context_invalid"
+
     NO_DESCRIPTIVE_CHANGE = "no_descriptive_change"
     INFERENTIAL_EVIDENCE_UNAVAILABLE = "inferential_evidence_unavailable"
     CONTEXT_INVALID = "context_invalid"
